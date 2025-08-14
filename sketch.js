7b5af41ff36eddbc834b3c3acc3f2c245f4e6d59
FlorenceTo <Florenceto@live.co.uk>
2025-08-14 12:04:16 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,118 +1,115 @@
 let shapes = [];
-let texts = [];
-let activeKeys = {};
-let sounds = {};
-let shapesMap = {};
 
 function setup() {
-  let cnv = createCanvas(windowWidth, windowHeight);
-  cnv.parent(document.body);
+  createCanvas(windowWidth, windowHeight, WEBGL);
   noStroke();
-  textAlign(CENTER, CENTER);
-  textSize(32);
-
-  texts.push({ text: "Press Keys", alpha: 255 });
-
-  userStartAudio(); // allow audio
-
-  // Define a simple set of keys with unique sounds and shape properties
-  let keys = [
-    'A','S','D','F','G','H','J','K','L','Q','W','E','R','T'
-  ];
-
-  keys.forEach((k, i) => {
-    // Each key gets a tap/bongo type sound
-    let osc = new p5.Oscillator(random(['triangle','sine','square']));
-    osc.start();
-    osc.amp(0);
-
-    let env = new p5.Envelope();
-    env.setADSR(0.001, 0.1, 0, 0.2);
-    env.setRange(random(0.3,0.6), 0);
-
-    sounds[k] = { osc, env };
-
-    // Each key also has a shape template
-    shapesMap[k] = {
-      sizeX: random(20,60),
-      sizeY: random(10,60),
-      rotationSpeed: random(-0.05,0.05),
-      fadeRate: random(1,3),
-      colors: [
-        [255,0,0],[0,255,0],[0,0,255],[255,255,0],
-        [255,128,0],[128,0,255],[0,255,255]
-      ]
-    };
-  });
 }
 
 function draw() {
-  background(0,50);
+  background(0);
+  rotateY(frameCount * 0.002); // slow rotation for scene
 
-  // Draw shapes
-  for (let i = shapes.length-1; i>=0; i--) {
+  for (let i = shapes.length - 1; i >= 0; i--) {
     let s = shapes[i];
-    fill(s.color[0],s.color[1],s.color[2],s.alpha);
+
+    // Move shape in 3D
+    s.x += s.vx;
+    s.y += s.vy;
+    s.z += s.vz;
+
+    // Modulate pitch
+    let freqOffset = sin(frameCount * 0.01 + s.offset) * 100;
+    s.osc.freq(s.baseFreq + freqOffset);
+
+    // Update 3D panner position
+    s.panner.setPosition(s.x / 2, s.y / 2, s.z / 2);
+
+    // Fade out
+    s.opacity -= 1;
+    if (s.opacity <= 0) {
+      s.osc.amp(0, 0.2);
+      s.osc.stop(0.2);
+      shapes.splice(i, 1);
+      continue;
+    }
+
+    // Draw shape
     push();
-    translate(s.x, s.y);
-    rotate(s.angle);
-    ellipse(0,0,s.sizeX,s.sizeY);
+    translate(s.x, s.y, s.z);
+    fill(s.color[0], s.color[1], s.color[2], s.opacity);
+    let size = map(s.baseFreq + freqOffset, 200, 900, 50, 200);
+
+    if (s.type === 'sphere') {
+      sphere(size);
+    } else {
+      box(size);
+    }
     pop();
-
-    s.alpha -= s.fadeRate;
-    s.angle += s.rotationSpeed;
-
-    if(s.alpha <=0) shapes.splice(i,1);
-  }
-
-  // Draw fading text
-  for(let t of texts){
-    fill(255,t.alpha);
-    text(t.text, width/2, 100);
-    if(t.alpha>0) t.alpha-=2;
   }
 }
 
 function keyPressed() {
-  if (!activeKeys[key]) {
-    activeKeys[key]=true;
+  userStartAudio();
 
-    let pan = map(mouseX,0,width,-1,1);
+  let type = random() < 0.5 ? 'sphere' : 'box';
+  let baseFreq = random(200, 800);
 
-    if(sounds[key]){
-      let s = sounds[key];
-      s.osc.freq(random(200,800));
-      s.osc.pan(pan);
-      s.env.play(s.osc);
-    }
+  let osc = new p5.Oscillator(type === 'sphere' ? 'triangle' : 'sine');
+  osc.freq(baseFreq);
+  osc.start();
+  osc.amp(0.5, 0.05);
 
-    if(shapesMap[key]){
-      let shp = shapesMap[key];
-      shapes.push({
-        x: random(width),
-        y: random(height),
-        sizeX: shp.sizeX,
-        sizeY: shp.sizeY,
-        color: random(shp.colors),
-        alpha: 255,
-        fadeRate: shp.fadeRate,
-        angle: random(TWO_PI),
-        rotationSpeed: shp.rotationSpeed
-      });
-    }
-  }
+  // small delay for metallic feel
+  let delay = new p5.Delay();
+  delay.process(osc, 0.2, 0.4, 2000);
+
+  // Create 3D panner and link to shape
+  let panner = new p5.Panner3D();
+  panner.pan(0, 0, 0); // initial center
+  osc.disconnect();
+  osc.connect(panner);
+
+  // Random color
+  let colors = [
+    [255, 0, 0],
+    [200, 0, 0],
+    [0, 255, 0],
+    [0, 200, 0],
+    [255, 255, 255],
+    [200, 200, 200]
+  ];
+  let color = random(colors);
+
+  // Random velocities
+  let vx = random(-1, 1);
+  let vy = random(-1, 1);
+  let vz = random(-1, 1);
+
+  shapes.push({
+    x: random(-width/2, width/2),
+    y: random(-height/2, height/2),
+    z: random(-500, 500),
+    vx: vx,
+    vy: vy,
+    vz: vz,
+    type: type,
+    baseFreq: baseFreq,
+    offset: random(TWO_PI),
+    osc: osc,
+    panner: panner,
+    color: color,
+    opacity: 255
+  });
 }
 
 function keyReleased() {
-  if(activeKeys[key]){
-    delete activeKeys[key];
+  if (shapes.length > 0) {
+    let s = shapes[shapes.length - 1];
+    s.osc.amp(0, 0.5);
+    s.osc.stop(0.5);
   }
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-}
-
-function mousePressed() {
-  userStartAudio();
 }
