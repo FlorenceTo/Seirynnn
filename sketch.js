9361a93e56520960da6f8b5417c790a247451822
FlorenceTo <Florenceto@live.co.uk>
2025-08-14 19:15:00 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,22 +2,23 @@ let shapes = [];
 let activeKeys = {};
 let chordOscs = [];
 
+// skipping C, starting from interval 3
 let keyMap = {
-  'D': { type:'sphere', freq: 261.63 }, // C4
-  'R': { type:'box',    freq: 293.66 }, // D4
-  'C': { type:'sphere', freq: 329.63 }, // E4
-  'F': { type:'box',    freq: 369.99 }, // F#4
-  'V': { type:'sphere', freq: 415.30 }, // G#4
-  'T': { type:'box',    freq: 466.16 }, // A#4
-  'G': { type:'sphere', freq: 493.88 }, // B4
-  'B': { type:'box',    freq: 523.25 }, // C5
-  'Y': { type:'sphere', freq: 554.37 }, // C#5
-  'H': { type:'box',    freq: 587.33 }, // D5
-  'N': { type:'sphere', freq: 622.25 }, // D#5
-  'U': { type:'box',    freq: 659.25 }, // E5
-  'J': { type:'sphere', freq: 698.46 }, // F5
-  'K': { type:'box',    freq: 739.99 }, // F#5
-  'M': { type:'sphere', freq: 783.99 }  // G5
+  'D': { type:'sphere', freq: 146.83 }, // D3
+  'R': { type:'box',    freq: 164.81 }, // E3
+  'C': { type:'sphere', freq: 185.00 }, // F#3
+  'F': { type:'box',    freq: 207.65 }, // G#3
+  'V': { type:'sphere', freq: 233.08 }, // A#3
+  'T': { type:'box',    freq: 246.94 }, // B3
+  'G': { type:'sphere', freq: 293.66 }, // D4
+  'B': { type:'box',    freq: 329.63 }, // E4
+  'Y': { type:'sphere', freq: 369.99 }, // F#4
+  'H': { type:'box',    freq: 415.30 }, // G#4
+  'N': { type:'sphere', freq: 466.16 }, // A#4
+  'U': { type:'box',    freq: 493.88 }, // B4
+  'J': { type:'sphere', freq: 587.33 }, // D5
+  'K': { type:'box',    freq: 659.25 }, // E5
+  'M': { type:'sphere', freq: 739.99 }  // F#5
 };
 
 function setup() {
@@ -26,18 +27,14 @@ function setup() {
 }
 
 function draw() {
-  background(0, 50); // subtle trail effect
+  background(0, 50);
 
   for (let s of shapes) {
-    // Move shapes
     s.x += s.vx;
     s.y += s.vy;
-
-    // Bounce off edges
     if (s.x < 0 || s.x > width) s.vx *= -1;
     if (s.y < 0 || s.y > height) s.vy *= -1;
 
-    // Draw shape with glow effect
     push();
     translate(s.x, s.y);
     for(let i=3;i>0;i--){
@@ -56,11 +53,8 @@ function keyPressed() {
   if (!keyMap[k]) return;
 
   userStartAudio();
-
-  // Add active key
   activeKeys[k] = keyMap[k];
 
-  // Spawn shape
   let config = keyMap[k];
   let s = {
     x: random(width), y: random(height),
@@ -71,15 +65,11 @@ function keyPressed() {
   };
   shapes.push(s);
 
-  // Handle chords: sphere+box
   let sphereKeys = Object.keys(activeKeys).filter(k=>activeKeys[k].type==='sphere');
   let boxKeys = Object.keys(activeKeys).filter(k=>activeKeys[k].type==='box');
 
   if(sphereKeys.length>0 && boxKeys.length>0){
-    // highest frequency of pressed keys
     let highestFreq = max([...sphereKeys,...boxKeys].map(k=>activeKeys[k].freq));
-
-    // Create 3-note chord (root + major 3rd + perfect 5th)
     chordOscs = [];
     [0,4,7].forEach(interval=>{
       let osc = new p5.Oscillator('triangle');
@@ -88,17 +78,13 @@ function keyPressed() {
       osc.start();
       chordOscs.push(osc);
     });
-  } else {
-    // no chord if one side missing
-    stopChord();
-  }
+  } else stopChord();
 }
 
 function keyReleased() {
   let k = key.toUpperCase();
   delete activeKeys[k];
 
-  // stop chord if either shape type released
   let sphereKeys = Object.keys(activeKeys).filter(k=>activeKeys[k].type==='sphere');
   let boxKeys = Object.keys(activeKeys).filter(k=>activeKeys[k].type==='box');
   if(sphereKeys.length===0 || boxKeys.length===0) stopChord();
