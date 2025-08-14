67f1f2de094f03168e303efc5c68d1c75fccae86
FlorenceTo <Florenceto@live.co.uk>
2025-08-14 19:16:09 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,23 +2,22 @@ let shapes = [];
 let activeKeys = {};
 let chordOscs = [];
 
-// skipping C, starting from interval 3
 let keyMap = {
-  'D': { type:'sphere', freq: 146.83 }, // D3
-  'R': { type:'box',    freq: 164.81 }, // E3
-  'C': { type:'sphere', freq: 185.00 }, // F#3
-  'F': { type:'box',    freq: 207.65 }, // G#3
-  'V': { type:'sphere', freq: 233.08 }, // A#3
-  'T': { type:'box',    freq: 246.94 }, // B3
-  'G': { type:'sphere', freq: 293.66 }, // D4
-  'B': { type:'box',    freq: 329.63 }, // E4
-  'Y': { type:'sphere', freq: 369.99 }, // F#4
-  'H': { type:'box',    freq: 415.30 }, // G#4
-  'N': { type:'sphere', freq: 466.16 }, // A#4
-  'U': { type:'box',    freq: 493.88 }, // B4
-  'J': { type:'sphere', freq: 587.33 }, // D5
-  'K': { type:'box',    freq: 659.25 }, // E5
-  'M': { type:'sphere', freq: 739.99 }  // F#5
+  'D': { type:'sphere', freq: 146.83 },
+  'R': { type:'box',    freq: 164.81 },
+  'C': { type:'sphere', freq: 185.00 },
+  'F': { type:'box',    freq: 207.65 },
+  'V': { type:'sphere', freq: 233.08 },
+  'T': { type:'box',    freq: 246.94 },
+  'G': { type:'sphere', freq: 293.66 },
+  'B': { type:'box',    freq: 329.63 },
+  'Y': { type:'sphere', freq: 369.99 },
+  'H': { type:'box',    freq: 415.30 },
+  'N': { type:'sphere', freq: 466.16 },
+  'U': { type:'box',    freq: 493.88 },
+  'J': { type:'sphere', freq: 587.33 },
+  'K': { type:'box',    freq: 659.25 },
+  'M': { type:'sphere', freq: 739.99 }
 };
 
 function setup() {
@@ -27,6 +26,7 @@ function setup() {
 }
 
 function draw() {
+  // trails
   background(0, 50);
 
   for (let s of shapes) {
@@ -37,13 +37,16 @@ function draw() {
 
     push();
     translate(s.x, s.y);
-    for(let i=3;i>0;i--){
-      fill(255, 50, 50*(i/3));
-      ellipse(0,0,s.size+i*5);
-    }
+
+    // glowing red pulse
+    let glow = sin(frameCount * 0.1) * 50 + 50;
+
+    stroke(255, 0, 0, glow);
+    strokeWeight(1);
     fill(255);
     if(s.type==='sphere') ellipse(0,0,s.size);
     else rectMode(CENTER), rect(0,0,s.size,s.size);
+
     pop();
   }
 }
@@ -61,41 +64,56 @@ function keyPressed() {
     vx: random(-1,1), vy: random(-1,1),
     type: config.type,
     size: 50,
-    freq: config.freq
+    freq: config.freq,
+    osc: new p5.Oscillator('triangle'),
+    startTime: millis()
   };
+  s.osc.freq(s.freq);
+  s.osc.amp(0.3, 0.01);
+  s.osc.start();
+  s.osc.mod = 0.002; // small wobble
+
   shapes.push(s);
 
-  let sphereKeys = Object.keys(activeKeys).filter(k=>activeKeys[k].type==='sphere');
-  let boxKeys = Object.keys(activeKeys).filter(k=>activeKeys[k].type==='box');
-
-  if(sphereKeys.length>0 && boxKeys.length>0){
-    let highestFreq = max([...sphereKeys,...boxKeys].map(k=>activeKeys[k].freq));
-    chordOscs = [];
-    [0,4,7].forEach(interval=>{
-      let osc = new p5.Oscillator('triangle');
-      osc.freq(highestFreq * Math.pow(2, interval/12));
-      osc.amp(0.4,0.01);
-      osc.start();
-      chordOscs.push(osc);
-    });
-  } else stopChord();
+  // check sphere+box for chord
+  updateChord();
 }
 
 function keyReleased() {
   let k = key.toUpperCase();
-  delete activeKeys[k];
+  let shapeIndex = shapes.findIndex(s => s.freq === keyMap[k]?.freq);
+  if(shapeIndex>=0){
+    let s = shapes[shapeIndex];
+    s.osc.amp(0,0.5);
+    s.osc.stop(0.5);
+    shapes.splice(shapeIndex,1);
+  }
 
-  let sphereKeys = Object.keys(activeKeys).filter(k=>activeKeys[k].type==='sphere');
-  let boxKeys = Object.keys(activeKeys).filter(k=>activeKeys[k].type==='box');
-  if(sphereKeys.length===0 || boxKeys.length===0) stopChord();
+  delete activeKeys[k];
+  updateChord();
 }
 
-function stopChord(){
+function updateChord(){
+  let sphereKeys = Object.keys(activeKeys).filter(k=>activeKeys[k].type==='sphere');
+  let boxKeys = Object.keys(activeKeys).filter(k=>activeKeys[k].type==='box');
+
+  // stop previous chord
   for(let o of chordOscs){
-    o.amp(0,0.5);
-    o.stop(0.5);
+    o.amp(0,0.3);
+    o.stop(0.3);
   }
-  chordOscs=[];
+  chordOscs = [];
+
+  if(sphereKeys.length>0 && boxKeys.length>0){
+    let highestFreq = max([...sphereKeys,...boxKeys].map(k=>activeKeys[k].freq));
+    [0,4,7].forEach(interval=>{
+      let osc = new p5.Oscillator('triangle');
+      osc.freq(highestFreq * Math.pow(2, interval/12));
+      osc.amp(0.2,0.01);
+      osc.start();
+      chordOscs.push(osc);
+    });
+  }
 }
 
 function windowResized(){
